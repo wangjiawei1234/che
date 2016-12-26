e243a8ab51e23ea9e2baf8ecab5c50b6ef04d097
WangJiawei1234 <923411364@qq.com>
2016-12-26 10:38:39 +0800

    baichewang

---
+++ js/car.js
@@ -0,0 +1,12 @@
+/*轮播图*/
+var pic = document.getElementById("pic");
+var imgs = ['1.jpg','2.jpg','3.jpg'];
+var i = 0;
+function my(){
+	pic.src = 'img/' + imgs[i];
+	i++;
+	if(i > imgs.length){
+		i = 0;
+	}
+}
+var time = setInterval('my()' , 1000);
